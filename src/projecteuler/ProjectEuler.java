39e1d108e05603fcdb05051020783fa329366430
fahuel <nahuelffunes@email.com>
2023-04-19 22:28:20 -0300

    Largest Product in a series

--- src/projecteuler/ProjectEuler.java
+++ src/projecteuler/ProjectEuler.java
@@ -13,9 +13,11 @@ public class ProjectEuler {
        System.out.println("Fifth Problem: "+SmallestMultiple.smallestNumber());
        SquareDifference sq=new SquareDifference();
        System.out.println("Sixth Problem : "+ sq.getDifference());
-       
        Primes pr=new Primes();
-        System.out.println("Seventh Problem "+pr.getValue(pr.primes10001()));
-        
+       System.out.println("Seventh Problem "+pr.getValue(pr.primes10001()));
+       
+       ProductSeries pro=new ProductSeries();
+       System.out.println("Eighth Problem: "+pro.productSeries);
+       
     }
 }
